ab1b7b7a58d60670d0acf538867294a127bfdc72
dom061077 <dom061077@0d437cea-b1ad-c05f-6bf1-ed9c53d5eec9>
2013-06-04 17:08:02 +0000

    nueva changelist

--- trunk/legacysaie/grails-app/domain/com/educacion/alumno/Alumno.groovy
+++ trunk/legacysaie/grails-app/domain/com/educacion/alumno/Alumno.groovy
@@ -1,12 +1,24 @@
 package com.educacion.alumno
 
 import com.alumno.alumno.TipoDocumentoIdentidad
+import com.educacion.enums.SexoEnum
+
 
 class Alumno {
     int id
     String numeroDocumento
     String apellido
     String nombre
+    SexoEnum sexo
+    java.sql.Date fechaNacimiento
+    String paisNacimiento
+    String provinciaNacimiento
+    String localidadNacimiento
+    String calleDomicilio
+    String numeroDomicilio
+    String barrioDomicilio
+
+
 
     TipoDocumentoIdentidad tipoDocumento
 
@@ -22,6 +34,13 @@ class Alumno {
         apellido column: 'apellido'
         nombre column: 'nombre'
         tipoDocumento column: 'tipodocumento'
+        fechaNacimiento column:  'fechanacimiento'
+        paisNacimiento (column: 'paisnacimiento')
+        provinciaNacimiento(column: 'provincianacimiento')
+        localidadNacimiento(column:'localidadnacimiento')
+        calleDomicilio(column: 'calledomicilio')
+        numeroDomicilio(column: 'numerodomicilio')
+        barrioDomicilio(column:  'barriodomiciolio')
 
 
 
