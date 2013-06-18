426da00f9cf2edf4fd97fa5dd586cadbf153f56f
dom061077 <dom061077@0d437cea-b1ad-c05f-6bf1-ed9c53d5eec9>
2013-06-18 12:33:14 +0000

    New changelist

---
+++ web-app/js/inscripcion/inscripcion.js
@@ -0,0 +1,101 @@
+Ext.onReady(function(){
+    Ext.QuickTips.init();
+    var viewport = new Ext.Viewport({
+        layout:'fit',
+        items:[{
+            xtype: 'grouptabpanel',
+            tabWidth: 130,
+            activeGroup: 0,
+            items: [{
+                mainItem: 1,
+                items: [{
+                    title: 'Tickets',
+                    layout: 'fit',
+                    iconCls: 'x-icon-tickets',
+                    tabTip: 'Tickets tabtip',
+                    style: 'padding: 10px;'//,
+                    //items: [new SampleGrid([0,1,2,3,4])]
+                },
+                    {
+                        xtype: 'portal',
+                        title: 'Dashboard',
+                        tabTip: 'Dashboard tabtip',
+                        items:[{
+                            columnWidth:.33,
+                            style:'padding:10px 0 10px 10px',
+                            items:[{
+                                title: 'Grid in a Portlet',
+                                layout:'fit'//,
+                                //tools: tools//,
+                                //items: new SampleGrid([0, 2, 3])
+                            },{
+                                title: 'Another Panel 1'//,
+                                //tools: tools//,
+                                //html: Ext.example.shortBogusMarkup
+                            }]
+                        },{
+                            columnWidth:.33,
+                            style:'padding:10px 0 10px 10px',
+                            items:[{
+                                title: 'Panel 2'//,
+                                //tools: tools//,
+                                //html: Ext.example.shortBogusMarkup
+                            },{
+                                title: 'Another Panel 2'//,
+                                //tools: tools//,
+                                //html: Ext.example.shortBogusMarkup
+                            }]
+                        },{
+                            columnWidth:.33,
+                            style:'padding:10px',
+                            items:[{
+                                title: 'Panel 3'//,
+                                //tools: tools//,
+                                //html: Ext.example.shortBogusMarkup
+                            },{
+                                title: 'Another Panel 3'//,
+                                //tools: tools//,
+                                //html: Ext.example.shortBogusMarkup
+                            }]
+                        }]
+                    }, {
+                        title: 'Subscriptions',
+                        iconCls: 'x-icon-subscriptions',
+                        tabTip: 'Subscriptions tabtip',
+                        style: 'padding: 10px;',
+                        layout: 'fit',
+                        items: [{
+                            xtype: 'tabpanel',
+                            activeTab: 1,
+                            items: [{
+                                title: 'Nested Tabs'//,
+                                //html: Ext.example.shortBogusMarkup
+                            }]
+                        }]
+                    }, {
+                        title: 'Users',
+                        iconCls: 'x-icon-users',
+                        tabTip: 'Users tabtip',
+                        style: 'padding: 10px;'//,
+                        //html: Ext.example.shortBogusMarkup
+                    }]
+            }, {
+                expanded: true,
+                items: [{
+                    title: 'Configuration',
+                    iconCls: 'x-icon-configuration',
+                    tabTip: 'Configuration tabtip',
+                    style: 'padding: 10px;'//,
+                    //html: Ext.example.shortBogusMarkup
+                }, {
+                    title: 'Email Templates',
+                    iconCls: 'x-icon-templates',
+                    tabTip: 'Templates tabtip',
+                    style: 'padding: 10px;'//,
+                    //html: Ext.example.shortBogusMarkup
+                }]
+            }]
+        }]
+    });
+
+});
