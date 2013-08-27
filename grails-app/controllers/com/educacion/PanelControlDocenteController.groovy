83be5b211dbed96b4d23611316b955d5b8134bf8
dom061077 <dom061077@0d437cea-b1ad-c05f-6bf1-ed9c53d5eec9>
2013-08-27 16:59:19 +0000

    New changelist

--- grails-app/controllers/com/educacion/PanelControlDocenteController.groovy
+++ grails-app/controllers/com/educacion/PanelControlDocenteController.groovy
@@ -2,6 +2,7 @@ package com.educacion
 
 import com.educacion.academico.examen.CargaExamen
 import grails.converters.JSON
+import com.educacion.academico.carrera.AnioLectivo
 
 class PanelControlDocenteController {
     def springSecurityService
@@ -31,6 +32,41 @@ class PanelControlDocenteController {
         returnMap.rows=recordList
         returnMap.success = true
         returnMap.total = 0
+        render returnMap as JSON
+
+    }
+
+    def cargaexamenlist(){
+        def returnMap = [:]
+        def recordList = [:]
+
+        /*def cargasexamenes = CargaExamen.createCriteria().list(){
+            docente{
+                eq("id",params.id.toString().toInteger())
+            }
+            anioLectivo{
+                eq("id",parmas.aniolectivo_id.toString().toInteger())
+            }
+        }
+        cargaexamenes.each {
+            recordList << [id: it.id,denominacion:it.den]
+            
+        } */
+        returnMap.success = true
+        returnMap.rows=recordList
+        render returnMap as JSON
+    }
+
+    def aniolectivonotas(){
+
+        def returnMap = [:]
+        def recordList = []
+        def anioslist = AnioLectivo.list()
+        anioslist.each{
+            recordList << [id:it.id,descripcion:it.descripcion]
+
+        }
+        returnMap.rows=recordList
         render returnMap as JSON
 
     }
