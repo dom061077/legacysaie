065fbac84ef2335911e0a254dc80dc3e9b56e988
dom061077 <dom061077@0d437cea-b1ad-c05f-6bf1-ed9c53d5eec9>
2013-10-22 17:00:14 +0000

    New changelist

--- trunk/legacysaie/grails-app/domain/com/educacion/administrativo/cobranza/CuponPago.groovy
+++ trunk/legacysaie/grails-app/domain/com/educacion/administrativo/cobranza/CuponPago.groovy
@@ -24,6 +24,43 @@ class CuponPago {
     Double importe3
     boolean pagado = false
     String codigoBarras
+
+    String getCodigoBarrasConvertido(){
+        int sumaDigitoVerif = 0;
+        int posicion = 1;
+        String textoFinal = "";
+        String texto = codigoBarras
+        
+        for (int i = texto.length() - 1; i > -1; i--)
+        {
+            //sumaDigitoVerif = sumaDigitoVerif + Convert.ToInt32(texto.Substring(i, 1)) * (posicion % 2 == 0 ? 1 : 3);
+            sumaDigitoVerif = sumaDigitoVerif + texto.substring(i,1).toInteger() * (posicion % 2 == 0 ? 1 : 3);
+            posicion++;
+        }
+        //aqui agrego el dígito verificador a la cadena original/
+        texto = texto + (sumaDigitoVerif % 10 == 0 ? 0 : (10 - sumaDigitoVerif % 10)).ToString();
+        //DigitoVerificador_tmp = (sumaDigitoVerif % 10 == 0 ? 0 : (10 - sumaDigitoVerif % 10)).ToString();
+
+        //la longitud de la cadena debe ser par
+        if (texto.length() % 2 != 0)
+            texto = "0" + texto;
+
+        //Convierto los caracteres
+
+        for (int i = 0; i < texto.Length; i += 2)
+        {
+            //if (Convert.ToInt32(texto.Substring(i, 2)) < 50)
+            if (Convert.ToInt32(texto.Substring(i, 2)) < 50)
+                textoFinal = textoFinal + (char)(Convert.ToInt32(texto.Substring(i, 2)) + 48);
+            else
+                textoFinal = textoFinal + (char)(Convert.ToInt32(texto.Substring(i, 2)) + 142);
+        }
+        textoFinal = "(" + textoFinal + ")";
+        return textoFinal;
+
+    }
+
+    static transients = ['codigoBarrasConvertido']
     
     static mapping = {
         version false
