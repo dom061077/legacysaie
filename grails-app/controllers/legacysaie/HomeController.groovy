fb6b16700e35929156b4efe127e670c2239da0c1
dom061077 <dom061077@0d437cea-b1ad-c05f-6bf1-ed9c53d5eec9>
2013-11-11 16:35:09 +0000

    nueva changelist

---
+++ grails-app/controllers/legacysaie/HomeController.groovy
@@ -0,0 +1,23 @@
+package legacysaie
+
+class HomeController {
+
+    def index() {
+        log.debug "INGRESO AL CONTROLLER HOME"
+        if (isNormal())
+            render(view: '/index')
+        else{
+            def userInstance = springSecurityService.currentUser
+            def controllerUri
+            if (userInstance.alumno)
+                controllerUri='panelControlm'
+            if (userInstance.docente)
+                controllerUri='panelControlDocente'
+            if (!userInstance.alumno && !userInstance.docente)
+                controllerUri='panelControlAdmin'
+            redirect(controller: controllerUri,action:'index')
+
+        }
+
+    }
+}
---
+++ test/unit/legacysaie/IndexControllerTests.groovy
@@ -0,0 +1,16 @@
+package legacysaie
+
+
+
+import grails.test.mixin.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(IndexController)
+class IndexControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
