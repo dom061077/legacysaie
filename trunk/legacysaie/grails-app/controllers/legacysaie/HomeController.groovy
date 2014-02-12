011c1e7ef9d5b7cbbe948c6dc8cb9566f3af49b1
dom061077 <dom061077@0d437cea-b1ad-c05f-6bf1-ed9c53d5eec9>
2014-02-12 15:18:14 +0000

    nueva changelist

--- trunk/legacysaie/grails-app/controllers/legacysaie/HomeController.groovy
+++ trunk/legacysaie/grails-app/controllers/legacysaie/HomeController.groovy
@@ -4,20 +4,27 @@ class HomeController {
     def springSecurityService
     def index() {
         log.debug "INGRESO AL CONTROLLER HOME"
-        if (isNormal())
+        def userInstance = springSecurityService.currentUser
+        def controllerUri
+
+        if (isNormal()){
             render(view: '/index')
-        else{
-            def userInstance = springSecurityService.currentUser
-            def controllerUri
+            if (userInstance.alumno)
+                controllerUri='panelControl'
+            if (userInstance.docente)
+                controllerUri='panelControlDocente'
+            if (!userInstance.alumno && !userInstance.docente)
+                controllerUri='panelControlAdmin'
+        }else{
+            userInstance = springSecurityService.currentUser
+            controllerUri
             if (userInstance.alumno)
                 controllerUri='panelControlm'
             if (userInstance.docente)
                 controllerUri='panelControlDocente'
             if (!userInstance.alumno && !userInstance.docente)
                 controllerUri='panelControlAdmin'
-            redirect(controller: controllerUri,action:'index')
-
         }
-
+        redirect(controller: controllerUri,action:'index')
     }
 }
